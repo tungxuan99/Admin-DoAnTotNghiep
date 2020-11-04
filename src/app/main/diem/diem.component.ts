22c0abc029892c179e87494b46e6684c51e789fa
tungxuan99 <70497455+tungxuan99@users.noreply.github.com>
2020-11-05 00:28:38 +0700

    update

--- src/app/main/diem/diem.component.ts
+++ src/app/main/diem/diem.component.ts
@@ -14,24 +14,44 @@ declare var $: any;
   styleUrls: ['./diem.component.css']
 })
 export class DiemComponent extends BaseComponent implements OnInit {
-  public giaoviens: any;
-  public giaovien: any;
+  public hocsinhs: any;
+  public lophocs: any;
   public totalRecords:any;
   public pageSize = 3;
   public page = 1;
   public uploadedFiles: any[] = [];
-  public formsearch: any;
+  public formds: any;
   public formdata: any;
   public doneSetupForm: any;  
   public showUpdateModal:any;
   public isCreate:any;
+  public check: any;
   submitted = false;
   @ViewChild(FileUpload, { static: false }) file_image: FileUpload;
   constructor(private fb: FormBuilder, injector: Injector) {
     super(injector);
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {this.formds = this.fb.group({
+    'MaLop': [''],
+    'Buoi': [''],     
+  });
+  this.check=true;
+  this.isCreate=false;
+  this._api.get('/api/lophoc/get-all').takeUntil(this.unsubscribe).subscribe(res => {
+    this.lophocs= res;
+    console.log(this.lophocs);
+    });
+  
+}
+get f() { return this.formdata.controls; }
 
+LayDS(){
+  this.check= false;
+  this.isCreate=true;
+  this._api.get('/api/hocsinh/get-by-lop/'+this.formds.get('MaLop').value).takeUntil(this.unsubscribe).subscribe(res => {
+    this.hocsinhs= res;
+    });
+
+}
 }
--- src/app/main/diemdanh/diemdanh.component.ts
+++ src/app/main/diemdanh/diemdanh.component.ts
@@ -14,17 +14,18 @@ declare var $: any;
   styleUrls: ['./diemdanh.component.css']
 })
 export class DiemdanhComponent extends BaseComponent implements OnInit {
-  public giaoviens: any;
-  public giaovien: any;
+  public hocsinhs: any;
+  public lophocs: any;
   public totalRecords:any;
   public pageSize = 3;
   public page = 1;
   public uploadedFiles: any[] = [];
-  public formsearch: any;
+  public formds: any;
   public formdata: any;
   public doneSetupForm: any;  
   public showUpdateModal:any;
   public isCreate:any;
+  public check: any;
   submitted = false;
   @ViewChild(FileUpload, { static: false }) file_image: FileUpload;
   constructor(private fb: FormBuilder, injector: Injector) {
@@ -32,6 +33,27 @@ export class DiemdanhComponent extends BaseComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.formds = this.fb.group({
+      'MaLop': [''],
+      'Buoi': [''],     
+    });
+    this.check=true;
+    this.isCreate=false;
+    this._api.get('/api/lophoc/get-all').takeUntil(this.unsubscribe).subscribe(res => {
+      this.lophocs= res;
+      console.log(this.lophocs);
+      });
+    
+  }
+  get f() { return this.formdata.controls; }
+
+  LayDS(){
+    this.check= false;
+    this.isCreate=true;
+    this._api.get('/api/hocsinh/get-by-lop/'+this.formds.get('MaLop').value).takeUntil(this.unsubscribe).subscribe(res => {
+      this.hocsinhs= res;
+      });
+
   }
 
 }
